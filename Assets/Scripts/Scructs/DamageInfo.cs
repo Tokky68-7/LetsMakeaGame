09bc49062c1e39dd118abfa2c4d629fd45ddb8ba
Tokky68-7 <karthikanlogan@gmail.com>
2026-06-29 20:50:59 +1000

    getting hitboxes/hurtboxes started

--- Assets/Scripts/Scructs/DamageInfo.cs
+++ Assets/Scripts/Scructs/DamageInfo.cs
@@ -3,5 +3,21 @@ using UnityEngine;
 public struct DamageInfo
 {
     public float Damage { get; }
-    public Vector2 Directio
+    public Vector2 Direction { get; }
+
+    public float Knockback {get; }
+    public GameObject Source { get; }
+
+    public DamageInfo(
+        float damage,
+        Vector2 direction,
+        float knockback,
+        GameObject source)
+    {
+        Damage = damage;
+        Direction = direction;
+        Knockback = knockback;
+        Source = source;
+    }
+    
 }
